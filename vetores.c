73daca2d54ad588c5375d3a76ae846c0e46bcbe9
laribrito <lary.29.ds@gmail.com>
2022-10-09 12:30:17 -0300

    feat: adiciona função na biblioteca de vetores

--- vetores.c
+++ vetores.c
@@ -15,7 +15,34 @@ void preencheVetor(int* vetor,int tam){
 void exibeVetor(int* vetor, int tam){
     printf("Vetor = ");
     for(int i=0; i<tam; i++) printf("%d ", vetor[i]);
+    printf("\n");
 }
 void adeusVetor(int* vetor){
     free(vetor);
+}
+
+int uniaoUnica(int *A, int n, int *B, int m, int *C){
+    int aux=1;
+    C=realloc(C,(aux)*sizeof(int));
+    C[0]=A[0];
+    for (int i = 0, j=0; i < n, j<m; i++, j++)
+    {
+        if(!(contido(A[i], C, aux))){
+            C=realloc(C,(++aux)*sizeof(int));
+            C[aux-1]=A[i];
+        }
+        if(!(contido(B[j], C, aux))){
+            C=realloc(C,(++aux)*sizeof(int));
+            C[aux-1]=B[j];
+        }
+    }
+    // printf("Aux: %d\n\n", aux);
+    return aux;
+}
+
+int contido(int elemento, int *vetor, int tam){
+    for(int x=0;x<tam; x++){
+        if (vetor[x]==elemento) return True;
+    }
+    return False;
 }
--- vetores.h
+++ vetores.h
@@ -6,4 +6,10 @@ void preencheVetor(int*,int);
 void exibeVetor(int*, int);
 void adeusVetor(int*);
 
+// EDGAR - UESC // A função deve retornar o tamanho do vetor C
+int uniaoUnica(int *A, int n, int *B, int m, int *C);
+
+// Retorna 1 se encontrar o elemento E no vetor V (vetor de tamanho T), 0 se não
+int contido(int E, int *V, int T);
+
 #endif
